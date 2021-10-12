2b74a9533aef9ca047edc71834e77c8a62c9f2da
Ma Xihan <31639301+MXHsj@users.noreply.github.com>
2021-10-11 23:34:54 -0400

    add o3d pc segmentation

--- scripts/GetRealSenseData.py
+++ scripts/GetRealSenseData.py
@@ -8,130 +8,130 @@ from pyrealsense2 import pyrealsense2 as rs
 
 class GetRealSenseData():
 
-    def __init__(self):
-        self.__pipeline = rs.pipeline()
-        self.__config = rs.config()
-        self.__config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        self.__config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-        # depth filter
-        self.__hole_filling = rs.hole_filling_filter()
-        self.__spat_filter = rs.spatial_filter()
-        self.__spat_filter.set_option(rs.option.filter_smooth_alpha, 0.5)
-        self.__spat_filter.set_option(rs.option.filter_smooth_delta, 10)
-        # process
-        self.__align_depth2color = rs.align(rs.stream.color)
-        self.__pc = rs.pointcloud()   # get pointcloud
-        # frame data
-        self.depth_image = None
-        self.depth_colormap = None
-        self.color_image = None
-        self.verts = None           # xyz
-        self.texcoords = None       # u,v
-        # start streaming
-        self.__pipeline.start(self.__config)
-        # get intrinsic
-        self.__profile = self.__pipeline.get_active_profile()
-        self.intr = self.__profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
+  def __init__(self):
+    self.__pipeline = rs.pipeline()
+    self.__config = rs.config()
+    self.__config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    self.__config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    # depth filter
+    self.__hole_filling = rs.hole_filling_filter()
+    self.__spat_filter = rs.spatial_filter()
+    self.__spat_filter.set_option(rs.option.filter_smooth_alpha, 0.5)
+    self.__spat_filter.set_option(rs.option.filter_smooth_delta, 10)
+    # process
+    self.__align_depth2color = rs.align(rs.stream.color)
+    self.__pc = rs.pointcloud()   # get pointcloud
+    # frame data
+    self.depth_image = None
+    self.depth_colormap = None
+    self.color_image = None
+    self.verts = None           # xyz
+    self.texcoords = None       # u,v
+    # start streaming
+    self.__pipeline.start(self.__config)
+    # camera intrinsic
+    self.__profile = self.__pipeline.get_active_profile()
+    self.intr = self.__profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
 
-    def stream_color_frame(self):
-        frames = self.__pipeline.wait_for_frames()
-        color_frame = frames.get_color_frame()
-        if color_frame:
-            # Convert images to numpy arrays
-            self.color_image = np.asanyarray(color_frame.get_data())
-        return color_frame
+  def stream_color_frame(self):
+    frames = self.__pipeline.wait_for_frames()
+    color_frame = frames.get_color_frame()
+    if color_frame:
+      # Convert images to numpy arrays
+      self.color_image = np.asanyarray(color_frame.get_data())
+    return color_frame
 
-    def stream_depth_frame(self):
-        frames = self.__pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        # apply depth filters
-        # depth_frame = self.__spat_filter.process(depth_frame)
-        # depth_frame = self.__hole_filling.process(depth_frame)
-        if depth_frame:
-            # Convert images to numpy arrays
-            self.depth_image = np.asanyarray(depth_frame.get_data())
-            self.depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(self.depth_image, alpha=0.03), cv2.COLORMAP_JET)
-        return depth_frame
+  def stream_depth_frame(self):
+    frames = self.__pipeline.wait_for_frames()
+    depth_frame = frames.get_depth_frame()
+    # apply depth filters
+    # depth_frame = self.__spat_filter.process(depth_frame)
+    # depth_frame = self.__hole_filling.process(depth_frame)
+    if depth_frame:
+      # Convert images to numpy arrays
+      self.depth_image = np.asanyarray(depth_frame.get_data())
+      self.depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(self.depth_image, alpha=0.03), cv2.COLORMAP_JET)
+    return depth_frame
 
-    def stream_depth2color_aligned(self):
-        frames = self.__pipeline.wait_for_frames()
-        # align depth to color frame
-        aligned_frames = self.__align_depth2color.process(frames)
-        depth_frame = aligned_frames.get_depth_frame()
-        color_frame = aligned_frames.get_color_frame()
-        # apply depth filters
-        # depth_frame = self.__spat_filter.process(depth_frame)
-        # depth_frame = self.__hole_filling.process(depth_frame)
-        if depth_frame and color_frame:
-            self.depth_image = np.asanyarray(depth_frame.get_data())
-            self.depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(self.depth_image, alpha=0.03), cv2.COLORMAP_JET)
-            self.color_image = np.asanyarray(color_frame.get_data())
-        return depth_frame, color_frame
+  def stream_depth2color_aligned(self):
+    frames = self.__pipeline.wait_for_frames()
+    # align depth to color frame
+    aligned_frames = self.__align_depth2color.process(frames)
+    depth_frame = aligned_frames.get_depth_frame()
+    color_frame = aligned_frames.get_color_frame()
+    # apply depth filters
+    # depth_frame = self.__spat_filter.process(depth_frame)
+    # depth_frame = self.__hole_filling.process(depth_frame)
+    if depth_frame and color_frame:
+      self.depth_image = np.asanyarray(depth_frame.get_data())
+      self.depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(self.depth_image, alpha=0.03), cv2.COLORMAP_JET)
+      self.color_image = np.asanyarray(color_frame.get_data())
+    return depth_frame, color_frame
 
-    def stream_pointcloud(self):
-        depth_frame, color_frame = self.stream_depth2color_aligned()
-        points = self.__pc.calculate(depth_frame)
-        self.__pc.map_to(depth_frame)
-        # Pointcloud data to arrays
-        v, t = points.get_vertices(), points.get_texture_coordinates()
-        self.verts = np.asanyarray(v).view(np.float32).reshape(-1, 3)   # xyz
-        self.texcoords = np.asanyarray(t).view(np.float32).reshape(-1, 2)  # uv
-        return depth_frame, color_frame
+  def stream_pointcloud(self):
+    depth_frame, color_frame = self.stream_depth2color_aligned()
+    points = self.__pc.calculate(depth_frame)
+    self.__pc.map_to(depth_frame)
+    # Pointcloud data to arrays
+    v, t = points.get_vertices(), points.get_texture_coordinates()
+    self.verts = np.asanyarray(v).view(np.float32).reshape(-1, 3)   # xyz
+    self.texcoords = np.asanyarray(t).view(np.float32).reshape(-1, 2)  # uv
+    return depth_frame, color_frame
 
-    def stop_stream(self):
-        self.__pipeline.stop()
+  def stop_stream(self):
+    self.__pipeline.stop()
 
 
 # exit
 def breakLoop(vis):
-    vis.destroy_window()
-    cv2.destroyAllWindows()
-    sys.exit()
+  vis.destroy_window()
+  cv2.destroyAllWindows()
+  sys.exit()
 
 
 # test case: visualize pointcloud using open3d
 def test():
-    get_realsense_data = GetRealSenseData()
+  get_realsense_data = GetRealSenseData()
 
-    pointcloud = o3d.geometry.PointCloud()
-    cam_intr = o3d.camera.PinholeCameraIntrinsic(
-        get_realsense_data.intr.width, get_realsense_data.intr.height,
-        get_realsense_data.intr.fx, get_realsense_data.intr.fy,
-        get_realsense_data.intr.ppx, get_realsense_data.intr.ppy)
-    vis = o3d.visualization.VisualizerWithKeyCallback()
-    vis.create_window("pointcloud", 640, 480)
-    vis.register_key_callback(ord("Q"), breakLoop)
-    isGeometryAdded = False
+  pointcloud = o3d.geometry.PointCloud()
+  cam_intr = o3d.camera.PinholeCameraIntrinsic(
+      get_realsense_data.intr.width, get_realsense_data.intr.height,
+      get_realsense_data.intr.fx, get_realsense_data.intr.fy,
+      get_realsense_data.intr.ppx, get_realsense_data.intr.ppy)
+  vis = o3d.visualization.VisualizerWithKeyCallback()
+  vis.create_window("pointcloud", 640, 480)
+  vis.register_key_callback(ord("Q"), breakLoop)
+  isGeometryAdded = False
 
-    cv2.namedWindow('RGB-depth', cv2.WINDOW_AUTOSIZE)
-    while True:
-        get_realsense_data.stream_depth2color_aligned()
-        color_depth_stack = np.vstack((get_realsense_data.color_image, get_realsense_data.depth_colormap))
-        cv2.imshow('RGB-depth', color_depth_stack)
-        key = cv2.waitKey(10)
-        if key & 0xFF == ord('Q') or key == 27:
-            get_realsense_data.stop_stream()
-            breakLoop(vis)
-            break
-        pointcloud.clear()
+  cv2.namedWindow('RGB-depth', cv2.WINDOW_AUTOSIZE)
+  while True:
+    get_realsense_data.stream_depth2color_aligned()
+    color_depth_stack = np.vstack((get_realsense_data.color_image, get_realsense_data.depth_colormap))
+    cv2.imshow('RGB-depth', color_depth_stack)
+    key = cv2.waitKey(10)
+    if key & 0xFF == ord('Q') or key == 27:
+      get_realsense_data.stop_stream()
+      breakLoop(vis)
+      break
 
-        depth = o3d.geometry.Image(get_realsense_data.depth_image)
-        rgb = cv2.cvtColor(get_realsense_data.color_image, cv2.COLOR_RGB2BGR)
-        color = o3d.geometry.Image(rgb)
+    pointcloud.clear()
+    depth = o3d.geometry.Image(get_realsense_data.depth_image)
+    rgb = cv2.cvtColor(get_realsense_data.color_image, cv2.COLOR_RGB2BGR)
+    color = o3d.geometry.Image(rgb)
 
-        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
-            color, depth, convert_rgb_to_intensity=False)
-        pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, cam_intr)
-        pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-        pointcloud += pcd
-        if not isGeometryAdded:
-            vis.add_geometry(pointcloud)
-            isGeometryAdded = True
-        # update geometry
-        vis.update_geometry(pointcloud)
-        vis.poll_events()
-        vis.update_renderer()
+    rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
+        color, depth, convert_rgb_to_intensity=False)
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, cam_intr)
+    pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+    pointcloud += pcd
+    if not isGeometryAdded:
+      vis.add_geometry(pointcloud)
+      isGeometryAdded = True
+    # update geometry
+    vis.update_geometry(pointcloud)
+    vis.poll_events()
+    vis.update_renderer()
 
 
 if __name__ == "__main__":
-    test()
+  test()
--- scripts/OCT_scan_path_plan.py
+++ scripts/OCT_scan_path_plan.py
@@ -1,9 +1,134 @@
 #! /usr/bin/env python3
+import sys
 import rospy
 from cv2 import cv2
+import open3d as o3d
 import selectinwindow
 from GetRealSenseData import *
 
-realsense = GetRealSenseData()
+# Set recursion limit
+sys.setrecursionlimit(10 ** 9)
 
+
+def generateWaypoints(rec_obj):
+  num_vert_pnts = int(rec_obj.outRect.h*0.08)
+  num_hori_pnts = int(rec_obj.outRect.w*0.05)
+  vert_pnt_dist = int(rec_obj.outRect.h/(num_vert_pnts+1))
+  hori_pnt_dist = int(rec_obj.outRect.w/(num_hori_pnts+1))
+  wp = list()
+  for row in range(num_vert_pnts):
+    for col in range(num_hori_pnts):
+      if row % 2 == 0:
+        wp.append([rec_obj.outRect.x+hori_pnt_dist*(col+1),
+                   rec_obj.outRect.y+vert_pnt_dist*(row+1)])
+      else:
+        wp.append([rec_obj.outRect.x+hori_pnt_dist*(num_hori_pnts+1) -
+                   hori_pnt_dist*(col+1),
+                   rec_obj.outRect.y+vert_pnt_dist*(row+1)])
+  return wp
+
+
+def visualizeWaypoints(img, waypoints):
+  for i in range(len(waypoints)):
+    cv2.circle(img, tuple(waypoints[i]), 2, (0, 255, 0))
+
+
+# exit
+def breakLoop(vis):
+  vis.destroy_window()
+  cv2.destroyAllWindows()
+  sys.exit()
+
+
+realsense = GetRealSenseData()
 cv2.namedWindow('RGB-depth', cv2.WINDOW_AUTOSIZE)
+ROI_rec = selectinwindow.DragRectangle(realsense.color_image, 'RGB-depth', 640, 480)
+cv2.setMouseCallback(ROI_rec.wname, selectinwindow.dragrect, ROI_rec)
+wp = None
+# open3d stuff
+pointcloud = o3d.geometry.PointCloud()
+outlier_cloud = o3d.geometry.PointCloud()
+
+cam_intr = o3d.camera.PinholeCameraIntrinsic(
+    realsense.intr.width, realsense.intr.height,
+    realsense.intr.fx, realsense.intr.fy,
+    realsense.intr.ppx, realsense.intr.ppy)
+vis = o3d.visualization.VisualizerWithKeyCallback()
+vis.create_window("pointcloud", 640, 480)
+vis.register_key_callback(ord("Q"), breakLoop)
+isGeometryAdded = False
+isUpdateStream = True
+
+try:
+  while True:
+    key = cv2.waitKey(33)
+    if key & 0xFF == ord('q') or key == 27:
+      print("quit")
+      breakLoop(vis)
+      break
+    elif key == ord('\r'):
+      print("Dragged rectangle coordinates")
+      print(ROI_rec.outRect.x, ROI_rec.outRect.y,
+            ROI_rec.outRect.w, ROI_rec.outRect.h)
+      wp = generateWaypoints(ROI_rec)
+      print("waypoints: ", len(wp))
+    elif key == ord('c'):
+      print("clear ROI selection")
+      ROI_rec.resetRec()
+    elif key == ord('f'):
+      isUpdateStream = not isUpdateStream
+      if not isUpdateStream:
+        print("stop streaming")
+      else:
+        isGeometryAdded = False
+        print("resume streaming")
+    if isUpdateStream:
+      # stream color & depth aligned frames
+      realsense.stream_depth2color_aligned()
+      cv2.imshow('RGB-depth', realsense.color_image)
+      if wp is not None:
+        visualizeWaypoints(realsense.color_image, wp)
+
+      # ROI selection in color frame
+      ROI_rec.updateImg(realsense.color_image)
+      selectinwindow.clearCanvasNDraw(ROI_rec)
+
+    if not isUpdateStream:
+      if not isGeometryAdded:
+        # create open3d pointcloud
+        vis.remove_geometry(pointcloud)
+        depth = o3d.geometry.Image(realsense.depth_image)
+        rgb = cv2.cvtColor(realsense.color_image, cv2.COLOR_RGB2BGR)
+        color = o3d.geometry.Image(rgb)
+        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
+            color, depth, convert_rgb_to_intensity=False)
+        pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, cam_intr, project_valid_depth_only=True)
+        pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+        pointcloud = pcd
+        del pcd
+        # pointcloud.uniform_down_sample(every_k_points=100)
+
+        # segment plane
+        plane_model, inliers = pointcloud.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=100)
+        [a, b, c, d] = plane_model
+        print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
+        outlier_cloud = pointcloud.select_by_index(inliers, invert=True)
+        outlier_cloud.paint_uniform_color([91/255, 94/255, 186/255])
+
+        # estimate surface normal (search range 0.01m, search neighbors 10)
+        outlier_cloud.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.01, max_nn=10))
+        # coord = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.05, origin=pointcloud.get_center())
+
+        # o3d visualization
+        vis.add_geometry(pointcloud)
+        # vis.add_geometry(coord)
+        vis.add_geometry(outlier_cloud)
+        isGeometryAdded = True
+      # vis.update_geometry(pointcloud)
+      # vis.update_geometry(coord)
+      # vis.update_geometry(outlier_cloud)
+    vis.poll_events()
+    vis.update_renderer()
+
+finally:
+  realsense.stop_stream()
